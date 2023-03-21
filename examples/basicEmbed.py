6239abbc5787e54f26a30daee04574676c2c41e9
mac lawson <macattackairman@gmail.com>
2023-03-21 09:20:26 -0400

    new additions

---
+++ examples/basicEmbed.py
@@ -0,0 +1,4 @@
+# Author: Mac Lawson
+# Year: 2023
+# Description: Basic embedding example from a raw dataset
+
--- examples/datagathering.py
+++ examples/datagathering.py
@@ -1,18 +1,22 @@
+# Author: Mac Lawson
+# Year: 2023
+# Description: Data gathering with Wikipedia
+
 # Import the wikipedia library
 import wikipedia
 
-# Define the topic you want to search for on Wikipedia
-search_topic = "Python (programming language)"
+def searchWikipedia(term):
+    # Define the topic you want to search for on Wikipedia
+    search_topic = str(term)
 
-# Use the wikipedia library to search for the topic
-search_results = wikipedia.search(search_topic)
+    # Use the wikipedia library to search for the topic
+    search_results = wikipedia.search(search_topic)
 
-# Choose the first search result
-page_title = search_results[0]
+    # Choose the first search result
+    page_title = search_results[0]
 
-# Use the wikipedia library to get the page for the chosen search result
-page = wikipedia.page(page_title)
+    # Use the wikipedia library to get the page for the chosen search result
+    page = wikipedia.page(page_title)
 
-# Print out the page's title and summary
-print("Title: ", page.title)
-print("Summary: ", page.summary)
+    # Print out the page's title and summary
+    print(page.summary)
